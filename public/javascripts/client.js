1c3fb66ff911a1e29c33437b32e65ec81378fc05
Brian <bshslowfood@gmail.com>
2013-10-23 15:29:51 -0600

    solution including handlebars, clean-up

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -4,46 +4,30 @@ $(document).ready(function() {
 	var dataTemplate = Handlebars.compile(source);
 
 	$location = $('.location')
-	$title = $('#title')
-	$blurb = $('#blurb')
+	$repo = $('#repo')
+	$button = $('#button')
 	$location.attr("location","Seville")
-	
-	$location.on("click", function(){
+
+	$repo.on("click", ".location", function(){
+		
+		//set button to this
+		$location = $(this)
 		$.get('/location', function(data){
-		console.log("click!");
+			console.log("click!");
 
-		
-		
-		var currentLocation = $location.attr("location")
-		var locationIndex = 6
-		// console.log("currentLocation", currentLocation)
-		for (var i = 0; i < data.length; i++) {
-			// console.log("object",data[i])
-			// console.log("title",data[i]["title"])
-			if(data[i]["title"]===currentLocation){
-				
-				locationToAdd = data[i].next
-				locationIndex = i
-				
-			}
-		};
-		// console.log("dfgs",currentLocObj)
-		// console.log(locationToAdd)
-		// console.log(locationIndex)
+			var currentLocation = $location.attr("location")
 
-		// console.log("locationIndex",locationIndex)
-		// console.log(typeof(classToAdd))
-		// console.log(data[locationIndex-1])
-		$location.attr("location",locationToAdd)
-		if(locationIndex===0){
-			$title.html(data[0].title)
-			$blurb.html(data[0].blurb)
-		}
-		$title.html(data[locationIndex].title)
-		$blurb.html(data[locationIndex].blurb)
+			for (var i = 0; i < data.length; i++) {
 
-	});
+				if(data[i]["title"]===currentLocation){	
+					locationIndex = i;
+				}
+			};
 
+			var output = data[locationIndex];
+
+			$repo.html(dataTemplate(output));
+			$('#loc').attr("location",output.next)
+		});
 	})
-	
 });
--- routes/locations.js
+++
@@ -1,9 +0,0 @@
-
-/*
- * GET users listing.
- */
-
-// exports.list = function(req, res){
-//   res.send("<h1>Seville</h1>");
-//   res.redirect('/seville')
-// };
